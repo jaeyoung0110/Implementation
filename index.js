a6a6e176cd6fbc2a71f2f23a18abfa79b64ae3f7
JaeYoung Cho <cho.jaeyoung@thezooom.kr>
2024-10-22 15:47:35 +0900

    feat(Antd): 버튼 5가지 타입 구현

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const btn = document.querySelector("#primary");
+const btns = document.querySelectorAll("button");
+const rippleBtnClasses = ["primary", "default", "dashed"];
 
-btn.addEventListener("click", function (event) {
-  let rippleElement = document.createElement("span");
-  btn.appendChild(rippleElement);
-  setTimeout(() => {
-    rippleElement.remove();
-  }, 500);
+btns.forEach((btn) => {
+  // 클래스가 primary, default dashed 인 경우에만 ripple 적용하기
+  const isRippled = rippleBtnClasses.some((item) => {
+    return btn.classList.contains(item);
+  });
+
+  if (!isRippled) return;
+
+  btn.addEventListener("click", function (event) {
+    let rippleElement = document.createElement("span");
+    btn.appendChild(rippleElement);
+    setTimeout(() => {
+      rippleElement.remove();
+    }, 500);
+  });
 });
